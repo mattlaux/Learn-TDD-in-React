ef1db78fb04a6d44625d68e39897f17188100c31
drattylight <mlaux6@outlook.com>
2022-02-22 15:53:32 -0600

    Add NewMessageForm component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { default as NewMessageForm } from './components/NewMessageForm';
 
 const App = () => {
   return (
     <div>
+      <NewMessageForm />
     </div>
   );
 };
---
+++ src/components/NewMessageForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NewMessageForm = () => {
+    return (
+        <div>
+            <input 
+                type="text"
+                data-testid="messageText"
+            />
+            <button
+                data-testid="sendButton"
+            >
+                Send
+            </button>
+        </div>
+    );
+};
+
+export default NewMessageForm;
