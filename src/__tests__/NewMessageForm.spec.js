dd3bc29cbd8ec20fbc1528cda2cd11bfc0dfb354
drattylight <mlaux6@outlook.com>
2022-02-22 18:15:58 -0600

    Add test to check input clear after submit

--- src/__tests__/NewMessageForm.spec.js
+++ src/__tests__/NewMessageForm.spec.js
@@ -4,15 +4,15 @@ import userEvent from '@testing-library/user-event';
 import NewMessageForm from "../components/NewMessageForm";
 
 describe('<NewMessageForm />', () => {
-    let getByTestId;
 
-    describe('clicking the send button2', () => {
+    describe('clicking the send button', () => {
         
         test('clears the text field', () => {
             render(<NewMessageForm />);
             userEvent.click(screen.getByTestId('sendButton'));
             expect(screen.getByTestId('messageText').value).toEqual('');
         })
+
     })
     
 });
